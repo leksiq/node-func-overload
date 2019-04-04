93907fb6daf7b04791a11c38c1b50ec94610b33c
Alexey Zakharov <leksi@leksi.net>
2019-04-04 11:25:31 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+(() => {
+
+    function apply_condition(cond, v) {
+        if(cond === undefined) {
+            return true;
+        }
+        if(typeof(cond) === 'function') {
+            return cond(v);
+        }
+        return typeof(v) === cond.toString();
+    }
+
+    function M(v, cond) {
+        return (v !== undefined && apply_condition(cond, v) ? undefined : true);
+    }
+    
+    function X(v) {
+        return v;
+    }
+    
+    function C(v, cond) {
+        return apply_condition(cond, v) ? undefined : true;
+    }
+    
+    function satisfy(arr) {
+        return arr.every(v => {return v === undefined;});
+    }
+    
+    module.exports = {
+        M: M,
+        C: C,
+        X: X,
+        satisfy: satisfy
+    };
+    
+
+})();
